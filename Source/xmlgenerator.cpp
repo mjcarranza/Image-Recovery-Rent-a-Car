23e4b28706a994d046256527546f9b0b639d5758
Abner2111 <abneraq73@gmail.com>
2021-11-12 11:11:32 -0600

    Update xml creation

--- Source/xmlgenerator.cpp
+++ Source/xmlgenerator.cpp
@@ -1,19 +1,25 @@
+//
+// Created by abner on 12/11/21.
+//
+
 #include "xmlgenerator.h"
 #include <string>
 
 using namespace std;
-void xmlGenerator::newIndividual(int r, int g, int b){
-    TiXmlElement * element = new TiXmlElement("Individual");
+void xmlgenerator::newIndividual(int r, int g, int b){
+
+    TiXmlElement* element = new TiXmlElement("Individual");
+    population_root->LinkEndChild(element);
+
     string individual = "r: "+to_string(r)+","+"g: "+to_string(g)+",""b "+to_string(b);
-    TiXmlText * text = new TiXmlText(individual.c_str());
+    TiXmlText* text = new TiXmlText(individual.c_str());
     element->LinkEndChild(text);
-    currPopulation.RootElement()->LinkEndChild(element);
+
 }
-void xmlGenerator::pushPopulation(){
-    string directory = "generations/population"+to_string(pop_number)+".xml";
+void xmlgenerator::pushPopulation(){
+    string directory = "bal.xml";
+    //string directory = "population"+to_string(pop_number)+".xml";
     currPopulation.SaveFile(directory.c_str());
-    currPopulation.RootElement()->Clear();
+    population_root->Clear();
     pop_number++;
 }
-    
-
--- Source/xmlgenerator.h
+++ Source/xmlgenerator.h
@@ -1,21 +1,29 @@
+//
+// Created by abner on 12/11/21.
+//
 #ifndef XMLGENERATOR_H
 #define XMLGENERATOR_H
 #include "tinyxml.h"
 
 /**
  * @brief Class for generating XML with the data of the populations
- * 
+ *
  */
-class xmlGenerator
+class xmlgenerator
 {
 public:
-    xmlGenerator(){
-        TiXmlDeclaration* decl = new TiXmlDeclaration("1.0","","");
+    xmlgenerator(){
+        TiXmlDeclaration* decl = new TiXmlDeclaration("1.0","UTF-8","");
         currPopulation.LinkEndChild(decl);
+
+        TiXmlElement* population = new TiXmlElement("Population");
+        currPopulation.LinkEndChild(population);
+
+        population_root = population;
     }
     /**
      * @brief adds an individual to the population element in the xml document
-     * 
+     *
      * @param r red value
      * @param g green value
      * @param b blue value
@@ -24,12 +32,13 @@ public:
 
     /**
      * @brief loads the xml document into an xml file with the population number
-     * 
+     *
      */
     void pushPopulation();
 
 private:
     int pop_number;
+    TiXmlElement* population_root;
     TiXmlDocument currPopulation;
 };
 
