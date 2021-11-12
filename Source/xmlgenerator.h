cfa6555f0d9f3c24fa0aa7b8ec8a1bc3305c7684
Abner2111 <abneraq73@gmail.com>
2021-11-12 09:13:24 -0600

    documentation for xmlGenerator

--- Source/xmlgenerator.h
+++ Source/xmlgenerator.h
@@ -2,6 +2,10 @@
 #define XMLGENERATOR_H
 #include "tinyxml.h"
 
+/**
+ * @brief Class for generating XML with the data of the populations
+ * 
+ */
 class xmlGenerator
 {
 public:
@@ -9,7 +13,19 @@ public:
         TiXmlDeclaration* decl = new TiXmlDeclaration("1.0","","");
         currPopulation.LinkEndChild(decl);
     }
+    /**
+     * @brief adds an individual to the population element in the xml document
+     * 
+     * @param r red value
+     * @param g green value
+     * @param b blue value
+     */
     void newIndividual(int r, int g, int b);
+
+    /**
+     * @brief loads the xml document into an xml file with the population number
+     * 
+     */
     void pushPopulation();
 
 private:
