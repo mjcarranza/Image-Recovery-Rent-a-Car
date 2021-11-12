60dc1311ad3bdf25cbdfb124cea69d4e1b8e0eb5
Abner2111 <abneraq73@gmail.com>
2021-11-12 08:53:18 -0600

    XML generator ready

--- Source/xmlgenerator.cpp
+++ Source/xmlgenerator.cpp
@@ -1,6 +1,19 @@
 #include "xmlgenerator.h"
+#include <string>
 
-xmlGenerator::xmlGenerator()
-{
-    
+using namespace std;
+void xmlGenerator::newIndividual(int r, int g, int b){
+    TiXmlElement * element = new TiXmlElement("Individual");
+    string individual = "r: "+to_string(r)+","+"g: "+to_string(g)+",""b "+to_string(b);
+    TiXmlText * text = new TiXmlText(individual.c_str());
+    element->LinkEndChild(text);
+    currPopulation.RootElement()->LinkEndChild(element);
 }
+void xmlGenerator::pushPopulation(){
+    string directory = "generations/population"+to_string(pop_number)+".xml";
+    currPopulation.SaveFile(directory.c_str());
+    currPopulation.RootElement()->Clear();
+    pop_number++;
+}
+    
+
--- Source/xmlgenerator.h
+++ Source/xmlgenerator.h
@@ -1,17 +1,20 @@
 #ifndef XMLGENERATOR_H
 #define XMLGENERATOR_H
-
+#include "tinyxml.h"
 
 class xmlGenerator
 {
 public:
-    xmlGenerator();
+    xmlGenerator(){
+        TiXmlDeclaration* decl = new TiXmlDeclaration("1.0","","");
+        currPopulation.LinkEndChild(decl);
+    }
     void newIndividual(int r, int g, int b);
     void pushPopulation();
 
 private:
-    int ind_number;
     int pop_number;
+    TiXmlDocument currPopulation;
 };
 
 #endif // XMLGENERATOR_H
