8ec318e039c5211cf2f18583556b1e624dd29bc0
Abner2111 <abneraq73@gmail.com>
2021-11-12 06:59:37 -0600

    Added xml libraries

--- Source/xmlgenerator.cpp
+++ Source/xmlgenerator.cpp
@@ -2,5 +2,5 @@
 
 xmlGenerator::xmlGenerator()
 {
-
+    
 }
--- Source/xmlgenerator.h
+++ Source/xmlgenerator.h
@@ -6,6 +6,12 @@ class xmlGenerator
 {
 public:
     xmlGenerator();
+    void newIndividual(int r, int g, int b);
+    void pushPopulation();
+
+private:
+    int ind_number;
+    int pop_number;
 };
 
 #endif // XMLGENERATOR_H
